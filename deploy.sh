e27b3d435b1966a131dc2f148168d6e928d7da3f
Mayank Jain <mayank@helpshift.com>
2015-08-25 01:58:50 +0530

    WIP

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "Deploying blog post"
-git add . && git commit -am "WIP" && git push;
+git add .
+git commit -am "WIP"
+git push
 cd resources/public &&  git add . && git commit -am "WIP" && git push && cd ../../
 open http://firesofmay.github.io
